f4d0c735f9d9ae40c5f56747327cb12033fe6ab4
Ayoola <onaolapo.ayo@gmail.com>
2018-12-13 10:03:39 +0400

    base commit

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+      <div className="clearfix" id="footer">
+        <p> - +230-58531705 - Copyright 2018 - House MAsters - All rights reserved - </p>
+      </div>
+    )
+}
+
+export default Footer
