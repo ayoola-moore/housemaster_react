24f7cc9c0f729c2430d28d71bfb6780cf8bbe992
Ayoola <onaolapo.ayo@gmail.com>
2018-12-14 08:52:27 +0400

    all

---
+++ build/precache-manifest.95b84b64e58eeae82464d1fc45bdd8c7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b902883ba9c9b4f92123fa3e687a387",
+    "url": "/housemaster_react/static/media/paper_bg.4b902883.png"
+  },
+  {
+    "revision": "d2c4aa88203e7212d25b362b2573e75c",
+    "url": "/housemaster_react/static/media/notify_me.d2c4aa88.png"
+  },
+  {
+    "revision": "f48645efc6facc311479de1316a10bbd",
+    "url": "/housemaster_react/static/media/logo.f48645ef.png"
+  },
+  {
+    "revision": "7e34d6aa38bbc473e8cb709ce6aaf291",
+    "url": "/housemaster_react/static/media/bg.7e34d6aa.jpg"
+  },
+  {
+    "revision": "da75567a2901a57969c9967e3fa7219a",
+    "url": "/housemaster_react/static/media/aftermsg_bg.da75567a.png"
+  },
+  {
+    "revision": "6de0e4249720ca9e534e",
+    "url": "/housemaster_react/static/js/runtime~main.6de0e424.js"
+  },
+  {
+    "revision": "7a8a93cb9b53b5805388",
+    "url": "/housemaster_react/static/js/main.7a8a93cb.chunk.js"
+  },
+  {
+    "revision": "6522a19cc9aa27f9c691",
+    "url": "/housemaster_react/static/js/1.6522a19c.chunk.js"
+  },
+  {
+    "revision": "7a8a93cb9b53b5805388",
+    "url": "/housemaster_react/static/css/main.64535398.chunk.css"
+  },
+  {
+    "revision": "120d6de5e61cb873876dccadd7fb1cce",
+    "url": "/housemaster_react/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ebf11e69f9a4e65397bd6534d1b33800.js"
+  "/housemaster_react/precache-manifest.95b84b64e58eeae82464d1fc45bdd8c7.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/housemaster_react/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
